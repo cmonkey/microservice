40ccbf42d903cfccd2609f89aea0a3ecc86a2264
CMonkey <42.codemonkey@gmail.com>
2016-12-09 16:51:08 +0800

    add CyclicBarrier

---
+++ provider-user/src/main/java/com/microservice/service/CyclicBarrierDemo.java
@@ -0,0 +1,45 @@
+package com.microservice.service;
+
+import java.util.concurrent.CyclicBarrier;
+
+// 适用于只有多个线程都到达预定点时才可以继续执行
+// CyclicBarrier(int num) 等待线程的数量
+// CyclicBarrier(int num, Runnable action) 等待线程的数量以及所有线程到达后的操作
+// await() 到达临界点后暂停线程
+public class CyclicBarrierDemo{
+
+    public static void main (String [] args) {
+
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, new Runnable(){
+            @Override
+            public void run(){
+                System.out.println("Game start");
+            }
+        });
+
+        new Flayer("A", cyclicBarrier).start();
+        new Flayer("B", cyclicBarrier).start();
+        new Flayer("C", cyclicBarrier).start();
+    }
+
+}
+
+class Flayer extends Thread{
+    private CyclicBarrier cyclicBarrier;
+
+    public Flayer(String name, CyclicBarrier cyclicBarrier){
+        super(name);
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run(){
+        System.out.println(getName() + " is waiting other players......");
+
+        try {
+            cyclicBarrier.await();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
