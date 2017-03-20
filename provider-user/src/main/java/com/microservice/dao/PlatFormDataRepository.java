b775b3ddca68c1a48979d0bb27792a88d896694a
CMonkey <42.codemonkey@gmail.com>
2017-03-21 00:49:57 +0800

    add PlatFormDataRepository to provider-user project

---
+++ provider-user/src/main/java/com/microservice/dao/PlatFormDataRepository.java
@@ -0,0 +1,13 @@
+package com.microservice.dao;
+
+import com.microservice.entity.PlatFormData;
+import com.microservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by cmonkey on 12/6/16.
+ */
+@Repository
+public interface PlatFormDataRepository extends JpaRepository<PlatFormData,Long> {
+}
