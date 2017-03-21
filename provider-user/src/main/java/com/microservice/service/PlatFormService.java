f31a0f447f1510012c3b659411d6c29a48495d36
CMonkey <42.codemonkey@gmail.com>
2017-03-21 15:58:11 +0800

    refator PlatFormService

--- provider-user/src/main/java/com/microservice/service/PlatFormService.java
+++ provider-user/src/main/java/com/microservice/service/PlatFormService.java
@@ -4,6 +4,12 @@ import com.microservice.config.Constants;
 import com.microservice.dao.PlatFormDataRepository;
 import com.microservice.entity.PlatFormData;
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.BeanUtilsBean;
+import org.apache.commons.beanutils.ConvertUtils;
+import org.apache.commons.beanutils.ConvertUtilsBean;
+import org.apache.commons.beanutils.converters.DateConverter;
+import org.apache.commons.beanutils.converters.DateTimeConverter;
+import org.apache.commons.beanutils.locale.converters.DateLocaleConverter;
 import org.joda.time.DateTime;
 import org.joda.time.Days;
 import org.joda.time.format.DateTimeFormat;
@@ -19,10 +25,8 @@ import redis.clients.jedis.Jedis;
 
 import java.lang.reflect.InvocationTargetException;
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.joda.time.Days;
@@ -43,48 +47,7 @@ public class PlatFormService {
     @Autowired
     PlatFormDataRepository platFormDataRepository;
 
-    public List<PlatFormData> getPlatFormData(boolean isReadDb, DateTime startDt, DateTime endDt){
-
-        if(isReadDb){
-            List<PlatFormData> list = platFormDataRepository.findAll();
-
-            if(Collections3.isNotEmpty(list)){
-                for (PlatFormData data: list) {
-                    Date createTime = data.getCreateTime();
-                    DateTime dt = new DateTime(createTime.getTime());
-                    final int days = Days.daysBetween(defaultDt, dt).getDays();
-
-                    jedisShardedTemplate.execute(PLAT_FORM_DATA_KEY, new JedisTemplate.JedisActionNoResult(){
-
-                        @Override
-                        public void action(Jedis jedis) {
-
-                            String objectKey = PLAT_FORM_DATA_OBJECT_KEY + days;
-
-                            jedis.zadd(PLAT_FORM_DATA_KEY, days, objectKey);
-
-                            addCache(jedis, objectKey, Constants.id, data.getId());
-                            addCache(jedis, objectKey, Constants.annualSum, data.getAnnualSum());
-                            addCache(jedis, objectKey, Constants.bankCount, data.getBankCount());
-                            addCache(jedis, objectKey, Constants.cashSum, data.getCashSum());
-                            addCache(jedis, objectKey, Constants.cashWithSum, data.getCashWithSum());
-                            addCache(jedis, objectKey, Constants.collectSum, data.getCollectSum());
-                            addCache(jedis, objectKey, Constants.createTime, data.getCreateTime());
-                            addCache(jedis, objectKey, Constants.interestSum, data.getInterestSum());
-                            addCache(jedis, objectKey, Constants.investConvert, data.getInvestConvert());
-                            addCache(jedis, objectKey, Constants.investCount, data.getInvestCount());
-                            addCache(jedis, objectKey, Constants.investCountOrDay, data.getInvestCountOrDay());
-                            addCache(jedis, objectKey, Constants.originInvestCount, data.getOriginInvestCount());
-                            addCache(jedis, objectKey, Constants.originInvestSum, data.getOriginInvestSum());
-                            addCache(jedis, objectKey, Constants.redSum, data.getRedSum());
-                            addCache(jedis, objectKey, Constants.regCount,data.getRegCount());
-                        }
-                    });
-                }
-            }
-
-            return list;
-        }
+    public List<PlatFormData> getPlatFormData(DateTime startDt, DateTime endDt, int offset, int count){
 
         final int startDays = Days.daysBetween(defaultDt, startDt).getDays();
         final int endDays = Days.daysBetween(defaultDt, endDt).getDays();
@@ -95,7 +58,7 @@ public class PlatFormService {
 
                 List<PlatFormData> list = Lists.newArrayList();
 
-                Set<String> set = jedis.zrevrangeByScore(PLAT_FORM_DATA_KEY, endDays, startDays);
+                Set<String> set = jedis.zrevrangeByScore(PLAT_FORM_DATA_KEY, endDays, startDays, offset, count);
 
                 if(Collections3.isNotEmpty(set)){
 
@@ -126,6 +89,10 @@ public class PlatFormService {
         PlatFormData data = new PlatFormData();
 
         try {
+            DateConverter converter = new DateConverter();
+            converter.setPattern("yyyy-MM-dd HH:mm:ss.SSS");
+            ConvertUtils.register(converter, Date.class);
+
             BeanUtils.populate(data, map);
         } catch (IllegalAccessException e) {
             logger.error("getCacheData Exception = {}", e.getMessage());
@@ -136,5 +103,46 @@ public class PlatFormService {
         }
 
         return data;
+    }
+
+    public void loadPlatformData() {
+        List<PlatFormData> list = platFormDataRepository.findAll();
+
+        if(Collections3.isNotEmpty(list)) {
+            list.stream().forEach(data -> {
+
+                    Date createTime = data.getCreateTime();
+                    DateTime dt = new DateTime(createTime.getTime());
+                    final int days = Days.daysBetween(defaultDt, dt).getDays();
+
+                    jedisShardedTemplate.execute(PLAT_FORM_DATA_KEY, new JedisTemplate.JedisActionNoResult() {
+
+                    @Override
+                    public void action(Jedis jedis) {
+
+                        String objectKey = PLAT_FORM_DATA_OBJECT_KEY + days;
+
+                        jedis.zadd(PLAT_FORM_DATA_KEY, days, objectKey);
+
+                        addCache(jedis, objectKey, Constants.id, data.getId());
+                        addCache(jedis, objectKey, Constants.annualSum, data.getAnnualSum());
+                        addCache(jedis, objectKey, Constants.bankCount, data.getBankCount());
+                        addCache(jedis, objectKey, Constants.cashSum, data.getCashSum());
+                        addCache(jedis, objectKey, Constants.cashWithSum, data.getCashWithSum());
+                        addCache(jedis, objectKey, Constants.collectSum, data.getCollectSum());
+                        addCache(jedis, objectKey, Constants.createTime, data.getCreateTime());
+                        addCache(jedis, objectKey, Constants.interestSum, data.getInterestSum());
+                        addCache(jedis, objectKey, Constants.investConvert, data.getInvestConvert());
+                        addCache(jedis, objectKey, Constants.investCount, data.getInvestCount());
+                        addCache(jedis, objectKey, Constants.investCountOrDay, data.getInvestCountOrDay());
+                        addCache(jedis, objectKey, Constants.originInvestCount, data.getOriginInvestCount());
+                        addCache(jedis, objectKey, Constants.originInvestSum, data.getOriginInvestSum());
+                        addCache(jedis, objectKey, Constants.redSum, data.getRedSum());
+                        addCache(jedis, objectKey, Constants.regCount, data.getRegCount());
+                    }
+                });
+            });
+
+        }
     }
 }
