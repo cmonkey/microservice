cf8bd0c1e4dc65c3f7b91795af82d03572a99145
CMonkey <42.codemonkey@gmail.com>
2017-03-21 22:13:38 +0800

    add PlatFormFeignClient

---
+++ consumer-movie-feign/src/main/java/com/microservice/service/PlatFormFeignClient.java
@@ -0,0 +1,28 @@
+package com.microservice.service;
+
+import com.microservice.entity.PlatFormData;
+import com.microservice.entity.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * Created by cmonkey on 12/6/16.
+ */
+@FeignClient(name = "microservice-provider-user")
+public interface PlatFormFeignClient {
+
+    @RequestMapping("/plat_data/{startTime}/{endTime}/{offset}/{count}")
+    public List<PlatFormData> getPlatFormData(
+            @RequestParam("startTime") String startTime,
+            @RequestParam("endTime") String endTime,
+            @RequestParam("offset") int offset,
+            @RequestParam("count") int count
+            );
+
+    @RequestMapping(value = "/plat_data/refresh", method = RequestMethod.PUT)
+    public boolean refreshPlatFormData();
+}
