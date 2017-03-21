a6024453682011135e7ac0d53b48edcefdf43bd3
CMonkey <42.codemonkey@gmail.com>
2017-03-21 15:57:53 +0800

    change getPlatFormData add loadPlatformData for PlatFormDataController

--- provider-user/src/main/java/com/microservice/controller/PlatFormDataController.java
+++ provider-user/src/main/java/com/microservice/controller/PlatFormDataController.java
@@ -7,6 +7,7 @@ import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
@@ -23,17 +24,24 @@ public class PlatFormDataController {
     @Resource
     PlatFormService platFormService;
 
-    @GetMapping("/getPlatFormData/{isDb}/{startTime}/{endTime}")
+    @GetMapping("/getPlatFormData/{startTime}/{endTime}/{offset}/{count}")
     public List<PlatFormData> getPlatFormData(
-            @RequestParam(value = "isDb") boolean isDb,
             @RequestParam(value = "startTime") String startTime,
-            @RequestParam(value = "endTime") String endTime){
+            @RequestParam(value = "endTime") String endTime,
+            @RequestParam(value = "offset") int offset,
+            @RequestParam(value = "count") int count
+            ){
         //DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
         DateTimeFormatter fmt = DateTimeFormat.forPattern("yyyy-MM-dd");
 
         DateTime startDt = fmt.parseDateTime(startTime);
         DateTime endDt = fmt.parseDateTime(endTime);
 
-        return platFormService.getPlatFormData(isDb,startDt,endDt);
+        return platFormService.getPlatFormData(startDt,endDt,offset, count);
+    }
+
+    @PostMapping("/loadPlatformData")
+    public void loadPlatformData(){
+        platFormService.loadPlatformData();
     }
 }
