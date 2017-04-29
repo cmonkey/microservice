513668754ffff45e408bf4485e6f1336b12e9ed1
CMonkey <42.codemonkey@gmail.com>
2017-04-29 22:12:59 +0800

    通过scala 实现基于springboot 的restful api

--- provider-user/src/main/scala/com/microservice/controller/RESTFulController.scala
+++ provider-user/src/main/scala/com/microservice/controller/RESTFulController.scala
@@ -1,15 +1,22 @@
 package com.microservice.controller
 
+import java.time.format.DateTimeFormatter
+import java.util.List
 import java.util.concurrent.TimeUnit
+import javax.annotation.Resource
 
-import com.microservice.entity.User
-import org.springframework.web.bind.annotation.{GetMapping, RequestMapping, RequestParam, RestController}
+import com.microservice.entity.{PlatFormData, User}
+import com.microservice.service.PlatFormDataService
+import lombok.extern.slf4j.Slf4j
+import org.joda.time.format.DateTimeFormat
+import org.springframework.web.bind.annotation._
 
 import scala.beans.BeanProperty
 
 @RestController
 @RequestMapping(value = Array("/restful"))
-class RESTFulController {
+@Slf4j
+class RESTFulController @Resource()(val platFormDataService: PlatFormDataService ){
 
   @GetMapping(value = Array("/api/{id}"))
   def getId(): User = {
@@ -22,6 +29,24 @@ class RESTFulController {
     user.setAge(30)
 
     user
+  }
+
+  @PostMapping(value = Array("/getPlatFormData"))
+  def getPortalData(@RequestParam(value = "startTime") startTime: String,
+                    @RequestParam(value = "endTime") endTime: String,
+                    @RequestParam(value = "offset") offset: Int,
+                    @RequestParam(value = "count") count: Int
+                   ): List[PlatFormData] = {
+
+//    log.info("getPlatFormData params {}, {}, {}, {}", startTime, endTime, offset, count)
+    val fmt = DateTimeFormat.forPattern("yyyy-MM-hh HH:mm:ss")
+
+    val startDt = fmt.parseDateTime(startTime)
+    val endDt = fmt.parseDateTime(endTime)
+
+    val resultList = platFormDataService.getPlatFormData(startDt, endDt, offset, count)
+
+    resultList
   }
 }
 
